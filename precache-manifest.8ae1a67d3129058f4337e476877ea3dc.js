247be0ee8f9bb898ac32ab801802a6bea58833e9
Gavin Gosling <gavin.gosling1@gmail.com>
2020-06-21 22:40:00 -0400

    Updates

---
+++ precache-manifest.8ae1a67d3129058f4337e476877ea3dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0699d1d471904c8e16438f0681592e47",
+    "url": "/AnimatedSignatures/index.html"
+  },
+  {
+    "revision": "908a1e3ae653dcf0171b",
+    "url": "/AnimatedSignatures/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e55feebc631402a2623",
+    "url": "/AnimatedSignatures/static/css/main.ca13f1d4.chunk.css"
+  },
+  {
+    "revision": "908a1e3ae653dcf0171b",
+    "url": "/AnimatedSignatures/static/js/2.b28cfcea.chunk.js"
+  },
+  {
+    "revision": "0d8c1ef8f6d4be1721aeb4af9c0cb606",
+    "url": "/AnimatedSignatures/static/js/2.b28cfcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e55feebc631402a2623",
+    "url": "/AnimatedSignatures/static/js/main.21e32052.chunk.js"
+  },
+  {
+    "revision": "09e6d81e160e91e5b729",
+    "url": "/AnimatedSignatures/static/js/runtime-main.5eb6f86b.js"
+  }
+]);
